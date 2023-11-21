4ee11ef90595d561440a0dcec09e46338627c3ea
Felipe Granvile <felipegranvile71@gmail.com>
2023-11-21 10:40:45 -0300

    melhoro

--- hash.go
+++ hash.go
@@ -13,8 +13,7 @@ type Hash struct {
 
 type VetorHash struct {
 	Dados_Usuario     *Dados
-	Nome              string
-	Referencia_Indice int
+	Verificador_colisao bool
 }
 
 type Dados struct {
@@ -22,7 +21,7 @@ type Dados struct {
 	Endereco string
 	Telefone string
 	Next     *Dados
-	Prev     *Dados
+	//Prev     *Dados
 }
 
 func main() {
@@ -32,10 +31,13 @@ func main() {
 	hash = CriaHash()
 
 	InserirDados(hash, "Felipe", "Rua", "Telefone")
+	InserirDados(hash, "Felipe", "Rua", "Telefone")
 	InserirDados(hash, "Gabriel", "Rua", "Telefone")
 	InserirDados(hash, "Jose", "Rua", "Telefone")
 	InserirDados(hash, "Ana", "Rua", "Telefone")
-	fmt.Println(hash)
+	fmt.Println(hash.Indices[6].Dados_Usuario.Next)
+	fmt.Println(hash.Indices[6])
+	fmt.Println(hash.Quantidade)
 
 }
 
@@ -43,7 +45,7 @@ func CriaHash() *Hash {
 
 	hash_Table := &Hash{Indices: nil, Quantidade: 0}
 
-	hash_Table.Indices = make([]VetorHash, 2) //Indices aponta agora para um slice
+	hash_Table.Indices = make([]VetorHash, 10) //Indices aponta agora para um slice
 	return hash_Table
 }
 
@@ -60,23 +62,45 @@ func Peso_strings(nome string, hash_tb *Hash) int {
 
 	Peso = Somatoria
 	retorno := Peso % (len(hash_tb.Indices) + 1)
-	fmt.Print("Nome: ", nome, "\n", "Peso: ", retorno, "\n\n")
+	//fmt.Print("Nome: ", nome, "\n", "Peso: ", retorno, "\n\n")
 	return retorno
 }
 
 func InserirDados(hash_table *Hash, Nome_input string, Endereco_input string, Telefone_input string) {
 
-	Indice_Original := Peso_strings(Nome_input, hash_table)
-
+	Indice := Peso_strings(Nome_input, hash_table)
+	
 	Informacoes := &Dados{Nome: Nome_input, Endereco: Endereco_input, Telefone: Telefone_input}
-	Indice := Indice_Original
-
-	if len(hash_table.Indices) <= Indice_Original {
-
-		temporary := make([]VetorHash, Indice_Original+1)
+	//Indice := Indice
+	
+	//fmt.Println("Indice:", Indice)
+	//fmt.Println("Length of Indices:", len(hash_table.Indices))
+	
+	if len(hash_table.Indices) <= Indice {
+		//fmt.Println("Resizing Indices...")
+		temporary := make([]VetorHash, len(hash_table.Indices)*2)
 		copy(temporary, hash_table.Indices)
 		hash_table.Indices = temporary
 	}
+	
+	Hash := &hash_table.Indices[Indice]
+
+	if Hash.Dados_Usuario == nil {
+		Hash.Dados_Usuario = Informacoes
+	} else {
+		current := Hash.Dados_Usuario
+		for current.Next != nil {
+			if(Informacoes.Nome != current.Nome){
+				Hash.Verificador_colisao = true
+			}
+			current = Hash.Dados_Usuario.Next
+		}
+		current.Next = Informacoes
+	}
+
+	hash_table.Quantidade++
+
+	/*
 
 	for hash_table.Indices[Indice].Dados_Usuario != nil && hash_table.Indices[Indice].Nome != Nome_input {
 		Indice++
@@ -97,5 +121,6 @@ func InserirDados(hash_table *Hash, Nome_input string, Endereco_input string, Te
 		hash_table.Indices[Indice_Original].Referencia_Indice = Indice_Original
 	}
 
-	hash_table.Quantidade++
+	hash_table.Quantidade++4
+	*/
 }
